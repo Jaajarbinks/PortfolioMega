2573003eec77894f1c77f8996189d49f9a74d828
Jaajarbinks <mr.larsen@windowslive.com>
2023-05-07 14:55:10 -0600

    fixed home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Home() {
   return (
     <div className="Home">
-      <div className="h1">
-        <h1>Home Page</h1>
-      </div>
+      <h1 className="h1">
+        Home Page
+      </h1>
       <p className="Home1">
         Welcome to my domain. If you happen to be lost, feel free to take a look
         around. You may navigate with the links above to see what I am about.
